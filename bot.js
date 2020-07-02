468a5ea1717fedd077f36e200bd264cbb1a11bc1
EyassSy <51035404+EyassSy@users.noreply.github.com>
2020-07-02 13:36:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,25 +13,12 @@ bot.catecories = fs.readdirSync("./commands/");
 ["command"].forEach(handler=>{ 
   require(`./handlers/${handler}`)(bot); 
 });
-bot.once('ready',()=>{ 
+bot.on('ready',()=>{ 
   bot.user.setActivity(`${prefix}help | Nox Bot!`,{type: "STREAMING", url: 'https://twitch.tv/idk'}) 
   console.log(`Hello! ${bot.user.username} is now online!!`) 
 }) 
 
-bot.on('message', async message=>{ 
-  if(message.author.bot) return; 
-  if(!message.content.startsWith(prefix)) return; 
-  if(!message.guild) return; 
-  if(!message.member) message.member = await message.guild.fetchMember(message); 
-  const args = message.content.slice(prefix.length).trim().split(/ +/g); 
-  const cmd = args.shift().toLowerCase(); 
-  if(cmd.length == 0 ) return; 
-  const command = bot.commands.get(cmd) 
-  if(!command) command = bot.commands.get(bot.aliases.get(cmd)); 
-  if(command)
 
-    command.run(bot,message,args)
-})
 
 bot.on("message", async message => {
 
