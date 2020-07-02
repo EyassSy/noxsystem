16e462b562b1530702e4fc561be7a98dbd1d6f4c
EyassSy <51035404+EyassSy@users.noreply.github.com>
2020-07-02 11:42:25 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-const {Collection, Discord, Message, Client} = require('discord.js');
+const Discord = require('discord.js');
 const fs = require('fs');
 const bot = new Client({
   disableMentions: "everyone",
@@ -45,6 +45,28 @@ bot.on('message', async message=>{
     command.run(bot,message,args)
   }
 
+})
+
+bot.on("message", async message => {
+
+  if(message.author.bot) return;
+  if(message.channel.type === 'dm') return;
+
+  if(message.content.startsWith(prefix)) {
+      const args = message.content.slice(prefix.length).trim().split(/ +/);
+
+      const command = args.shift().toLowerCase();
+
+      if(!bot.commands.has(command)) return;
+
+
+      try {
+          bot.commands.get(command).run(bot, message, args);
+
+      } catch (error){
+          console.error(error);
+      }
+  }
 })
 
   bot.login(process.env.token);
