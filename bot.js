56cb8141d886dff99e6c9b8be9925cf5c32c96ca
EyassSy <51035404+EyassSy@users.noreply.github.com>
2020-07-03 23:32:24 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,21 +2,15 @@ const {Collection, Discord, Message, Client, Util} = require('discord.js');
 const fs = require('fs');
 const bot = new Client({ disableEveryone: true }) 
 const youtube = new YouTube(process.env.GOOGLE_API_KEY)
+const YouTube = require('simple-youtube-api')
 const config = require('./config.json');
 const prefix = config.prefix;
 const token = config.token;
 const ytdl = require('ytdl-core')
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 const YouTube = require('simple-youtube-api')
-=======
+
 const youtube = new YouTube(process.env.GOOGLE_API_KEY)
->>>>>>> parent of 9e61f81... Revert "Update bot.js"
-=======
-
-const YouTube = require('simple-youtube-api')
-
->>>>>>> parent of 56cd088... Update bot.js
 const queue = new Map()
 const ms = require('ms');
 const { connect } = require('http2');
@@ -32,31 +26,21 @@ bot.on('ready',()=>{
   bot.user.setActivity(`${prefix}help | Nox Bot!`,{type: "STREAMING", url: 'https://twitch.tv/idk'}) 
   console.log(`Hello! ${bot.user.username} is now online!!`) 
 })
-
 bot.on("message", async message => {
-
   if(message.author.bot) return;
   if(message.channel.type === 'dm') return;
-
   if(message.content.startsWith(prefix)) {
       const args = message.content.slice(prefix.length).trim().split(/ +/);
-
       const command = args.shift().toLowerCase();
-
       if(!bot.commands.has(command)) return;
-
-
       try {
           bot.commands.get(command).run(bot, message, args);
-
       } catch (error){
           console.error(error);
       }
   }
 })
-
 //////////////////////////////////////////////////////////////////
-
 bot.on("message", message => {
   if (message.author.bot) return;
   if (message.content.startsWith(prefix + "help")) {
@@ -65,27 +49,17 @@ bot.on("message", message => {
         .send(
           `   
 \`General Commands\` 🌍
-
 ${prefix}help : to see all the available commands
 ${prefix}ping : shows the bot's ping
 ${prefix}avatar : Get your own or someone else's avatar
-
-
-
-
 \`Moderation Commands\` ✨
-
 ${prefix}giveaway : ${prefix}giveaway <time> <channel id> <prize>
 ${prefix}clear : deletes multiple messages
 ${prefix}ban : to ban a member from the server
 ${prefix}kick : to kick a member from the server
 ${prefix}dm : to make me send a message to someone in privite
 ${prefix}poll : Create a simple yes or no poll
-
-
-
 \`Music Commands\` 🎵
-
 ${prefix}play : plays music
 ${prefix}stop : stop playing music
 ${prefix}skip : skips the current playing song
@@ -93,7 +67,6 @@ ${prefix}pause : pauses playing music
 ${prefix}resume : resume playing music
 ${prefix}volume : changes the music volume
 ${prefix}np : shows the current playing song name
-
   `
         )
         .then(e => {
@@ -113,7 +86,6 @@ ${prefix}np : shows the current playing song name
         .send(
           `   
           \`General Commands\` 🌍
-
           ${prefix}help : to see all the available commands
           ${prefix}ping : shows the bot's ping
           ${prefix}avatar : Get your own or someone else's avatar
@@ -121,18 +93,13 @@ ${prefix}np : shows the current playing song name
           
           
           \`Moderation Commands\` ✨
-
           ${prefix}giveaway : ${prefix}giveaway <time> <channel id> <prize>
           ${prefix}clear : deletes multiple messages
           ${prefix}ban : to ban a member from the server
           ${prefix}kick : to kick a member from the server
           ${prefix}dm : to make me send a message to someone in privite
           ${prefix}poll : Create a simple yes or no poll
-
-
-
           \`Music Commands\` 🎵
-
           ${prefix}play : plays music
           ${prefix}stop : stop playing music
           ${prefix}skip : skips the current playing song
@@ -140,14 +107,6 @@ ${prefix}np : shows the current playing song name
           ${prefix}resume : resume playing music
           ${prefix}volume : changes the music volume
           ${prefix}np : shows the current playing song name
-
-
-
-
-
-
-
-
         `
           )
         .then(e => {
@@ -165,47 +124,61 @@ ${prefix}np : shows the current playing song name
     }
   }
 });
-
 //////////////////////////////////////////////////////////////////
 
 bot.on('message', async message => {
   if(message.author.bot) return
   if(!message.content.startsWith(prefix)) return
-
   const args = message.content.substring(prefix.length).split(" ")
   const searchString = args.slice(1).join(' ')
   const url = args[1] ? args[1].replace(/<(.+)>/g, '$1') : ''
   const serverQueue = queue.get(message.guild.id)
-
   if(message.content.startsWith(`${prefix}play`)) {
     const voiceChannel = message.member.voice.channel
     if(!voiceChannel) return message.channel.send("You need to be in a voice channel to play music")
     const permissions = voiceChannel.permissionsFor(message.client.user)
     if(!permissions.has('CONNECT')) return message.channel.send("I don\'t have permissions to connect to the voice channel")
     if(!permissions.has('SPEAK')) return message.channel.send("I don\'t have permissions to speak in the channel")
-
-    if(url.match(/^https?:\/\/(www.youtube.com|youtube.com)\/playlist(.*)$/)) {
-        const playlist = await youtube.getPlaylist(url)
-        const videos = await playlist.getVideos()
-        for (const video of Object.values(videos)) {
-          const video2 = await youtube.getVideoByID(video.id)
-          await handleVideo(video2, message, voiceChannel, true)
+    try {
+      var video = await youtube.getVideoByID(url)
+    } catch {
+        try {
+          var videos = await youtube.searchVideos(searchString, 1)
+          var video = await youtube.getVideoByID(videos[0].id)
+        } catch {
+            return message.channel.send("I couldn\'t find any search results")
         }
-        message.channel.send(`Playlist **${playlist.title}** has been added to the queue`)
-        return undefined
-    } else {
-      try {
-        var video = await youtube.getVideoByID(url)
-      } catch {
-          try {
-            var videos = await youtube.searchVideos(searchString, 1)
-            var video = await youtube.getVideoByID(videos[0].id)
-          } catch {
-              return message.channel.send("I couldn\'t find any search results")
-          }
-      }
-      return handleVideo(video, message, voiceChannel)
     }
+    const song = {
+        id: video.id,
+        title: Util.escapeMarkdown(video.title),
+        url: `https://youtube.com/watch?v=${video.id}`
+    }
+    if(!serverQueue) {
+      const queueConstruct = {
+        textChannel: message.channel,
+        voiceChannel: voiceChannel,
+        connection: null,
+        songs: [],
+        volume: 5,
+        playing: true
+      }
+      queue.set(message.guild.id, queueConstruct)
+      queueConstruct.songs.push(song) 
+      try {
+        var connection = await voiceChannel.join()
+        queueConstruct.connection = connection
+        play(message.guild, queueConstruct.songs[0])
+    } catch (error) {
+        console.log(`There was an error connecting to the voice channel: ${error}`)
+        queue.delete(message.guild.id)
+        return message.channel.send(`There was an error connecting to the voice channel: ${error}`)
+    }
+  } else {
+    serverQueue.songs.push(song)
+    return message.channel.send(`**${song.title}** has been added to the queue`)
+  }
+  return undefined
   } else if(message.content.startsWith(`${prefix}stop`)) {
     if(!message.member.voice.channel) return message.channel.send("You need to be in a voice channel to stop the music")
     if(!serverQueue) return message.channel.send("There is nothing playing")
@@ -237,7 +210,6 @@ bot.on('message', async message => {
      message.channel.send(`
 __**Song Queue:**__
 ${serverQueue.songs.Map(song => `**-** ${song.title}`).join('\n')}
-
 **Now playing:** ${serverQueue.songs[0].title}
         `, { split: true })
         return undefined
@@ -257,57 +229,14 @@ ${serverQueue.songs.Map(song => `**-** ${song.title}`).join('\n')}
      message.channel.send("I have now resumed the music for you")
      return undefined
    }
-   return undefined
 })
-
-async function handleVideo(video, message, voiceChannel, playlist = false) {
-  const serverQueue = queue.get(message.guild.id)
-  
-  const song = {
-    id: video.id,
-    title: Util.escapeMarkdown(video.title),
-    url: `https://youtube.com/watch?v=${video.id}`
-}
-
-if(!serverQueue) {
-  const queueConstruct = {
-    textChannel: message.channel,
-    voiceChannel: voiceChannel,
-    connection: null,
-    songs: [],
-    volume: 5,
-    playing: true
-  }
-  queue.set(message.guild.id, queueConstruct)
-
-  queueConstruct.songs.push(song) 
-
-  try {
-    var connection = await voiceChannel.join()
-    queueConstruct.connection = connection
-    play(message.guild, queueConstruct.songs[0])
-} catch (error) {
-    console.log(`There was an error connecting to the voice channel: ${error}`)
-    queue.delete(message.guild.id)
-    return message.channel.send(`There was an error connecting to the voice channel: ${error}`)
-}
-} else {
-serverQueue.songs.push(song)
-if(playlist) return undefined
-return message.channel.send(`**${song.title}** has been added to the queue`)
-    }
-    return undefined
-}
-
 function play(guild, song) {
   const serverQueue = queue.get(guild.id)
-
   if(!song) {
     serverQueue.voiceChannel.leave()
     queue.delete(guild.id)
     return
   }
-
   const dispatcher = serverQueue.connection.play(ytdl(song.url))
   .on('finish', () => {
     serverQueue.songs.shift()
@@ -317,10 +246,8 @@ function play(guild, song) {
     console.log(error)
   })
   dispatcher.setVolumeLogarithmic(serverQueue.volume / 5)
-
   serverQueue.textChannel.send(`Start Playing: **${song.title}**`)
 }
-
 //////////////////////////////////////////////////////////////////
 
   bot.login(process.env.token);
