1e2469dc7e8f2e8052328b6d8e8618bf00bfd33a
EyassSy <51035404+EyassSy@users.noreply.github.com>
2020-07-12 15:07:51 +0200

    .

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 module.exports = {
     name: "ban",
-    description: "Kicks a member from the server",
+    description: "bans a member from the server",
 
     async run (client, message, args) {
 
