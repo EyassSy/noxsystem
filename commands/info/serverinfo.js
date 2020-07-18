6cd5c973ce43d5f526604104012cf23d0c828c66
EyassSy <51035404+EyassSy@users.noreply.github.com>
2020-07-18 22:30:07 +0200

    Update serverinfo.js

--- commands/info/serverinfo.js
+++ commands/info/serverinfo.js
@@ -1,7 +1,10 @@
 const Discord = require("discord.js");
 
-module.exports.run = async (bot, message, args) => {
+module.exports.help = {
+    name: "serverinfo",
+    description: "Gathers information about the server.",
 
+run: async (bot, message, args) => {
   let serverembed = new Discord.RichEmbed()
   .setTitle("Server Information")
   .setColor("0ED4DA")
@@ -13,9 +16,4 @@ module.exports.run = async (bot, message, args) => {
 
   return message.channel.send(serverembed);
 }
-
-
-module.exports.help = {
-  name: "serverinfo",
-  description: "Gathers information about the server."
-}
+};
