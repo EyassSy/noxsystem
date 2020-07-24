c195bb1a21535d44cb99e2c2f73936842c614117
EyassSy <51035404+EyassSy@users.noreply.github.com>
2020-07-24 08:36:00 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -45,12 +45,12 @@ bot.on("guildMemberAdd", member =>{
 
 bot.on("guildMemberAdd", member => {
   const welcomeChannel = member.guild.channels.cache.find(channel => channel.name === 'welcome')
-  welcomeChannel.send (`Welcome! ${member}`)
+  welcomeChannel.send(`Welcome! ${member}`)
 })
 
 bot.on("guildMemberRemove", member => {
   const welcomeChannel = member.guild.channels.cache.find(channel => channel.name === 'goodbye')
-  welcomeChannel.send (`Goodbye! ${member}`)
+  welcomeChannel.send(`Goodbye! ${member}`)
 })
 
 //////////////////////////////////////////////////////////////////
