894dc1ea1b4d3b72bcf05fa6bbca547108fd05fb
EyassSy <51035404+EyassSy@users.noreply.github.com>
2020-07-29 18:45:54 +0200

    Update userinfo.js

--- commands/info/userinfo.js
+++ commands/info/userinfo.js
@@ -1,40 +1,40 @@
-const { MessageEmbed } = require("discord.js");
-const { formatDate } = require("../../functions");
+const Discord = require('discord.js');
+const moment = require('moment');
+
 module.exports = {
   name: "userinfo",
-  description: "Get your own or someone else's avatar",
+  description: "Shows the information of a member/user",
   usage: "[user mention]",
   category: "info",
-  run: async (bot, message, args) => {
-    let Embed = new MessageEmbed();
-    let roles = [];
-    if (!message.mentions.users.first()) {
-      message.member.roles.cache.forEach((role) => {
-        roles.push(role.name);
-      });
-      Embed.setTitle(`Your avatar!`);
-      Embed.setThumbnail(message.author.displayAvatarURL());
-      Embed.setColor(`RANDOM`);
-      Embed.setDescription(
-        `Joined: (MM/DD/YYYY) ${formatDate(message.member.joinedAt)}\nID: ${
-          message.author.id
-        }\nRoles: ${roles}`
-      );
-      return message.channel.send(Embed);
-    } else {
-      let User = message.mentions.members.first();
-      User.roles.cache.forEach((role) => {
-        roles.push(role.name);
-      });
-      Embed.setTitle(`${bot.users.cache.get(User.id).tag}'s avatar!`);
-      Embed.setThumbnail(bot.users.cache.get(User.id).displayAvatarURL());
-      Embed.setColor(`RANDOM`);
-      Embed.setDescription(
-        `Joined: (MM/DD/YYYY) ${formatDate(User.joinedAt)}\nID: ${
-          User.id
-        }\nRoles: ${roles}`
-      );
-      return message.channel.send(Embed);
-    }
-  },
+  
+    run: async (bot, message, args) => {
+      let userArray = message.content.split(" ");
+      let userArgs = userArray.slice(1);
+      let member = message.mentions.members.first() || message.guild.members.cache.get(userArgs[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === userArgs.slice(0).join(" ") || x.user.username === userArgs[0]) || message.member;
+  
+      if (member.presence.status === 'dnd') member.presence.status = 'Do Not Disturb';
+      if (member.presence.status === 'online') member.presence.status = 'Online';
+      if (member.presence.status === 'idle') member.presence.status = 'Idle';
+      if (member.presence.status === 'offline') member.presence.status = 'offline';
+  
+      let x = Date.now() - member.createdAt;
+      let y = Date.now() - message.guild.members.cache.get(member.id).joinedAt;
+      const joined = Math.floor(y / 86400000);
+  
+      const joineddate = moment.utc(member.joinedAt).format("dddd, MMMM Do YYYY, HH:mm:ss");
+      let status = member.presence.status;
+  
+      const userEmbed = new Discord.MessageEmbed()
+      .setAuthor(member.user.tag, member.user.displayAvatarURL())
+      .setTimestamp()
+      .setColor('BLUE')
+      .setImage(member.user.displayAvatarURL())
+      .addField("Member ID", member.id)
+      .addField('Roles', `<@&${member._roles.join('> <@&')}>`)
+      .addField("Account Created On:", ` ${moment.utc(member.user.createdAt).format("dddd, MMMM Do YYYY")}`, true) 
+      .addField('Joined the server At', `${joineddate} \n> ${joined} day(S) Ago`)
+      .addField("Status", status)
+  
+      message.channel.send(userEmbed);
+  }
 };
